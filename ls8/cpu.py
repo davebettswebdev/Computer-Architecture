a078d90e73649315c9abc03205f6d7e63229910e
Dave Betts <ibedave777@gmail.com>
2020-08-18 08:17:00 -0700

    constructor added to

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,8 +6,9 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = None
 
     def load(self):
         """Load a program into memory."""
